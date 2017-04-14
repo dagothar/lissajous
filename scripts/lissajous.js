440f58b01354a1fa193aab274c11157f0837e982
dagothar <dagothar@gmail.com>
2017-04-14 14:29:22 +0200

    lissajous

--- scripts/lissajous.js
+++ scripts/lissajous.js
@@ -1,6 +1,35 @@
 var Lissajous = (function() {
   
-  function Lissajous() {
+  function Lissajous(A, w, p) {
+    
+    //var A[3] = { 1.0, 1.0, 1.0 };
+    //var w[3] = { 1.0, 1.0, 1.0 };
+    //var p[3] = { 0.0, 0.0, 0.0 };
+    var A = A, w = w, p = p;
+    
+    
+    this.calculate = function(t) {
+      return {
+        x: A[0] * Math.sin(2*Math.PI*w[0]*t + p[0]),
+        y: A[1] * Math.sin(2*Math.PI*w[1]*t + p[1]),
+        z: A[2] * Math.sin(2*Math.PI*w[2]*t + p[2])
+      };
+    };
+    
+    
+    this.render = function(scene, renderer) {
+      var material = new THREE.LineBasicMaterial({ color: 0x00ff00, linewidth: 3 });
+      var geometry = new THREE.Geometry();
+      
+      for (var t = 0.0; t <= 2*Math.PI; t += 0.01) {
+        var p = this.calculate(t);
+        geometry.vertices.push(new THREE.Vector3(p.x, p.y, p.z));
+      }
+      
+      var line = new THREE.Line(geometry, material);
+    
+      scene.add(line);
+    };
     
   };
   
--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,42 @@
 $(document).ready(function() {
   
-  var canvas = $('#view').get(0);
-  
-  /* initialize THREEjs */
-  var scene     = new THREE.Scene();
-  var camera    = new THREE.PerspectiveCamera(75, 1.0*canvas.width/canvas.height, 0.1, 1000);
-  var renderer  = new THREE.WebGLRenderer({ antialias: true, canvas: canvas });
-  controls      = new THREE.OrbitControls(camera);
-  
-  scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
-   
-  camera.position.set(0, 0, 10);
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
-  
-  renderer.setSize(canvas.width, canvas.height);
-  renderer.setClearColor(0xffffff);
-  
-  controls.addEventListener('change', render);
-  
-  var light = new THREE.PointLight(0xffffff, 1.0);
-  scene.add(light);
-  light.position.set(0, 0, 10);
+  var canvas;
+  var scene;
+  var camera;
+  var renderer;
+  var controls;
+  var light;
   
   
-  function requestAnimationFrame(render) {
-    var material = new THREE.LineBasicMaterial({ color: 0xff00ff, linewidth: 5 });
-    var geometry = new THREE.Geometry();
-    geometry.vertices.push(new THREE.Vector3(Math.random(), 1, 0));
-    geometry.vertices.push(new THREE.Vector3(0, 0, 0));
-    geometry.vertices.push(new THREE.Vector3(1, 0, 0));
-    geometry.vertices.push(new THREE.Vector3(0, 0, 0));
-    geometry.vertices.push(new THREE.Vector3(0, 0, 1));
-    var line = new THREE.Line(geometry, material);
-    scene.add(line);
+  function initialize() {
+    canvas = $('#view').get(0);
+  
+    /* initialize THREEjs */
+    scene     = new THREE.Scene();
+    //camera    = new THREE.PerspectiveCamera(75, 1.0*canvas.width/canvas.height, 0.1, 1000);
+    camera    = new THREE.OrthographicCamera(-canvas.width/200, canvas.width/200, -canvas.height/200, canvas.height/200, -10, 10);
+    renderer  = new THREE.WebGLRenderer({ antialias: true, canvas: canvas });
+    controls  = new THREE.OrbitControls(camera);
+    
+    scene.fog = new THREE.FogExp2(0xcccccc, 0.002);
+     
+    camera.position.set(0, 0, 1);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+    
+    renderer.setSize(canvas.width, canvas.height);
+    renderer.setClearColor(0xffffff);
+    
+    controls.addEventListener('change', render);
+    
+    light = new THREE.PointLight(0xffffff, 1.0);
+    scene.add(light);
+    light.position.set(0, 0, 10);
+  };
+  
+  
+  function requestAnimationFrame(renderer) {
+    var curve = new Lissajous([ 1, 1, 1 ], [ 1, 2, 4 ], [ 0, 0, 0 ]);
+    curve.render(scene, renderer);
   };
   
   
@@ -47,5 +51,6 @@ $(document).ready(function() {
   };
   
   
+  initialize();
   animate();
 });
